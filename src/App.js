7471cb02056ab3df15fc0ec2a1732eb9e4bfea66
deepak-singh19 <deepak.singh.16csc@bml.edu.in>
2022-09-16 02:29:58 +0530

    finalCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState ,useEffect} from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import wordsToNumbers from "words-to-numbers";
+import NewsCards from "./components/NewsCards/newsCards";
+import useStyles from './styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const alankey ="3886ef766e016f777bf7d88827c3d6cd2e956eca572e1d8b807a3e2338fdd0dc/stage";
+// const newskey="16aa53c6fdc4425d89d499fcaea5d1d7";
+const App=()=>{
+
+    const[newsArticles, setNewsArticles] = useState([]);
+    const[activeArticle, setActiveArticle]= useState(-1);
+
+    const classes= useStyles();
+
+    useEffect(()=>{
+        alanBtn({
+            key:alankey,
+            onCommand:({ command, articles, number})=>{
+                if(command=== 'newHeadlines'){
+                    console.log(articles);
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                    
+                }
+                else if(command=== 'highlight'){
+                    setActiveArticle((prevActiveArticle)=> prevActiveArticle+1);
+
+                }
+                else if(command ==='open'){
+                    const parsedNumber =number.length>2 ?wordsToNumbers(number,{fuzzy:true}): number;
+                    const article= articles[parsedNumber-1];
+
+                    if(parsedNumber>20){
+                        alanBtn().playText('Please try again');
+                    }
+                    else if(article){
+                        
+                        // alanBtn().playText('Opening....');
+                        alanBtn().playText('Opening');
+                        window.open(article.url,'_blank');
+                    }
+
+                    
+                }
+            }
+        })
+
+    },[]);
+    return(
+        <div>
+            <div className={classes.logoContainer}>
+                <img  src="https://voicebot.ai/wp-content/uploads/2019/10/alan.jpg" className={classes.alanLogo} alt="alan logo"/>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/NewsCards/styles.js
@@ -0,0 +1,36 @@
+import { makeStyles } from '@mui/styles';
+
+// import { makeStyles } from '@material-ui/core/styles';
+
+const Styles = makeStyles({
+
+    conatiner: {
+
+        padding: '0.5%',
+        width: '100%',
+        margin: 0,
+
+    },
+
+    card: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '90%',
+        height: '45vh',
+        padding: '10%',
+        borderRadius: 10,
+        color: 'white',
+        marginLeft: '5%',
+
+    },
+
+    infoCard: {
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'center'
+    }
+});
+
+export default Styles;
